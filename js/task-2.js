d30d452b462e87314d329445a2d9e56a4c8cd79f
2106igor <148360745+2106igor@users.noreply.github.com>
2024-02-25 20:28:34 +0200

    up

--- js/task-2.js
+++ js/task-2.js
@@ -11,19 +11,21 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-  {
-    url:
-      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Elephant Beside on Baby Elephant',
-  },
-  {
-    url:
-      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Blue Geeen and Orange Parrot',
-  },
-  {
-    url:
-      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Zebras on Zebra',
-  },
 ];
+
+const gallery = document.querySelector('.gallery');
+const markup = images
+	.map(image => `<li><img class="pictures" src=${image.url} width = '400' alt='${image.alt}'></img></li>`)
+	.join('')
+
+  gallery.insertAdjacentHTML('beforeend', markup)
+  
+const pictures = document.querySelectorAll('.pictures')
+
+gallery.style.display = 'flex';
+gallery.style.flexWrap = 'column-reverse'
+gallery.style.listStyle = 'none'
+gallery.style.gap = '20px'
+gallery.style.justifyContent = 'center'
+
+console.log(gallery);
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeButton = document.querySelector('button.change-color')
+const body = document.querySelector('body')
+// const button = document.querySelector('button')
+const colorValue = document.querySelector('span.color')
+
+
+changeButton.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  // button.style.backgroundColor = color;
+  colorValue.textContent = color;
+})
